e856ec61e4f39015f99f2a884e4c8e999a5ba59e
Bartosz Kaszubowski <gosimek@gmail.com>
2025-11-22 17:23:50 +0100

    chore: setup automatic release process

---
+++ release.config.ts
@@ -0,0 +1,59 @@
+import type { GlobalConfig } from 'semantic-release';
+
+const rules = [
+  { type: 'feat', release: 'minor', title: 'New features' },
+  { type: 'fix', release: 'patch', title: 'Bug fixes' },
+  { type: 'refactor', release: 'patch', title: 'Code refactors' },
+  { type: 'docs', release: 'patch', title: 'Documentation changes' },
+  { type: 'chore', release: 'patch', title: 'Other changes' }
+];
+
+const sortMap = Object.fromEntries(rules.map((rule, index) => [rule.title, index]));
+
+const config: GlobalConfig = {
+  repositoryUrl: 'https://github.com/react-native-community/vscode-react-native-directory',
+  branches: ['main'],
+  tagFormat: '${version}',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { revert: true, release: 'patch' }
+        ]
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: rules.map(({ type, title }) => ({ type, section: title }))
+        },
+        writerOpts: {
+          commitGroupsSort: (a: Record<string, string>, z: Record<string, string>) =>
+            sortMap[a.title] - sortMap[z.title]
+        }
+      }
+    ],
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', { npmPublish: true }],
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore: create new release ${nextRelease.version}\n\n${nextRelease.notes}',
+        assets: ['package.json', 'CHANGELOG.md']
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        draftRelease: false
+      }
+    ]
+  ]
+};
+
+export default config;
